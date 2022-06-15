6347d717ade7df6ec2b676406074d5ec0b1218bf
Began <beganbajrami@gmail.com>
2022-06-15 21:38:17 +0200

    some ricing, i think

--- autostart_blocking.sh
+++ autostart_blocking.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 autowall 2 Beach
-picom -b
+picom --vsync --experimental-backends -m 1 -f -c --blur-strength 5 --xrender-sync-fence --no-ewmh-fullscreen -b
 setsid scripts/statusbar/status.sh >/dev/null 2>&1 < /dev/null &
