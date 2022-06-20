e60fc81c8bf2b8ad83c8fe4ccae6fb2a13752c6e
Began <beganbajrami@gmail.com>
2022-06-20 22:03:06 +0200

    deleted unneeded shell script

--- autostart_blocking.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-autowall -q "archlinux"
-picom --vsync --experimental-backends -m 1 -f -c --blur-strength 5 --xrender-sync-fence --no-ewmh-fullscreen -b
-setsid scripts/statusbar/status.sh &
