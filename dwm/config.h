47088b8b648cace96a5dd356b69de2ab5886f963
Began <beganbajrami@gmail.com>
2022-08-28 23:20:35 +0200

    icons

--- dwm/config.h
+++ dwm/config.h
@@ -48,7 +48,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "5", "6", "7", "", "" };
 
 static const char *tagsel[][2] = {
 	{ "#ffffff", "#ff0000" },
